483e751e4846560575d16c37821f393d4f9f7f2d
harsh56845 <56841891+harsh56845@users.noreply.github.com>
2025-04-01 23:23:05 +0530

    Create Ugly Number.java

---
+++ Easy/Ugly Number.java
@@ -0,0 +1,18 @@
+// problem Link :- https://leetcode.com/problems/ugly-number/?envType=problem-list-v2&envId=math
+// TC -> O(logN)
+class Solution {
+    public boolean isUgly(int n) {
+        if(n==0) return false;
+        while(n!=1){
+            if(n%2==0 || n%3==0 || n%5==0){
+                if(n%2==0) n/=2;
+                else if(n%3==0) n/=3;
+                else n/=5;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
